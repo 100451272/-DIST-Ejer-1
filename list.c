bfb974f2b2dbe5dca88b0c6a2de56bdd125f7c70
100451272 <100451272@alumnos.uc3m.es>
2023-03-25 19:09:09 +0100

    cambioos

--- list.c
+++ list.c
@@ -12,6 +12,12 @@ int init(List *l) {
 
 int set(List *l, int key, struct tupla *tupla){
 	struct Node *ptr;
+	struct tupla *tupla_node = (struct tupla *) malloc(sizeof(struct tupla));
+	tupla_node->clave = key;
+	strcpy(tupla_node->valor1,tupla->valor1);
+	tupla_node->valor2 = tupla->valor2;
+	tupla_node->valor3 = tupla->valor3;
+
 
 	ptr = (struct Node *) malloc(sizeof(struct Node));
 	if (ptr == NULL) 
@@ -19,14 +25,14 @@ int set(List *l, int key, struct tupla *tupla){
 
 	if (*l == NULL) {  // emtpy list
 		ptr->key = key;
-		ptr->tupla = &tupla;
+		ptr->tupla = tupla_node;
 		ptr->next = NULL;
 		*l = ptr;
 	}
 	else {
 		// insert in head
 		ptr->key = key;
-		ptr->tupla = &tupla;
+		ptr->tupla = tupla_node;
 		ptr->next = *l;
 		*l = ptr;
 	}
@@ -38,7 +44,7 @@ int set(List *l, int key, struct tupla *tupla){
 int get(List *l, int key, struct tupla *tupla){
 	List aux;
 
-	aux = l;	
+	aux = *l;	
 
 	while (aux!=NULL) {
 		if (aux->key == key) {
@@ -58,7 +64,7 @@ int printList(List l){
 	aux = l;
 
 	while(aux != NULL){
-		printf("Key=%d    1=%s	  2=%d 3=%ld\n", aux->key, aux->tupla->valor1,
+		printf("Key=%d    1=%s	  2=%d 3=%f\n", aux->key, aux->tupla->valor1,
 		aux->tupla->valor2, aux->tupla->valor3);
 		aux = aux->next;
 	}
